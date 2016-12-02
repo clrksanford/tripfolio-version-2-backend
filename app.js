123a472061b758c4c6cbe32df22594dc7caf239f
Clark Sanford <clrksanford@gmail.com>
2016-12-02 14:56:44 -0600

    Use cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.us(cors());
 
 app.use('/', routes);
 app.use('/trips', trips);
